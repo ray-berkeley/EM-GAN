b2f2cde1a3016ff7843f9c84cf2d9c1d2e92da6f
Ray <rberkele@ucsd.edu>
2024-02-12 18:52:38 -0800

    added parser Added parser to take trimmap and data dirs

--- sr_dataprep.py
+++ sr_dataprep.py
@@ -1,11 +1,21 @@
+import argparse
 import os
 import sys
 import subprocess
 import tempfile
 import numpy as np
 
-trimmap_dir="./"
-dataset_dir="./data_dir"
+parser = argparse.ArgumentParser(description="Parser for user-specifible dataset paths.")
+
+parser.add_argument("trimmap_dir", type=str, help="The trimmap directory")
+parser.add_argument("dataset_dir", type=str, help="The data directory")
+
+args = parser.parse_args()
+
+trimmap_dir = args.trimmap_dir
+dataset_dir = args.dataset_dir
+
+
 write_dir=os.path.join(dataset_dir,"sr_hr_lr")
 flagPath = os.path.join(dataset_dir,"flag_dir")
 
@@ -22,7 +32,7 @@ def fil_process_new(filArr,trim,id):
 			if(line.rstrip()==''):
 				continue
 			elif(line.startswith("m")):
-				continue   
+				continue
 			elif(line.startswith("#")):
 				if line.startswith("#M1"):
 					coords = line.strip().split()[2:5]
@@ -39,12 +49,12 @@ def fil_process_new(filArr,trim,id):
 				sr.write(sr_orig.readline())
 				sr.close()
 				sr_orig.close()
-				
+
 				li = line.split(',')
 				if(len(li)!=2*25*25*25):
 					print("breaking")
-					break				
-				
+					break
+
 				lr = open(os.path.join(write_dir,id+"_"+str(idx)+"_LR.situs"),"w")
 				lr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
 				lr.write(" ".join(li[0:25*25*25]))
@@ -54,7 +64,7 @@ def fil_process_new(filArr,trim,id):
 				hr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
 				hr.write(" ".join(li[25*25*25:2*25*25*25]))
 				hr.close()
-				
+
 				idx+=1
 sr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "SR" in x]
 print(dataset_dir)
@@ -63,7 +73,7 @@ print(dataset_dir)
 map_mrc={}
 
 for f in sr_imageFileNames:
-	
+
 	filname=f.split("/")[-1].split(".")[0]
 	key = filname.split("_")[1]
 	idx = int(filname.split("_")[2])
@@ -73,7 +83,7 @@ for f in sr_imageFileNames:
 		ARR = [0]*12100
 		ARR[idx] = f
 		map_mrc[key]=ARR
-		
+
 	else:
 		map_mrc[key][idx] = f
 for i in map_mrc:
