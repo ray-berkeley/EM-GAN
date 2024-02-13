5d2c727bdbf1889645cf2d2b0880954bfa2e2cfe
Ray <rberkele@ucsd.edu>
2024-02-12 19:38:36 -0800

    fixed linting/inconsistent indentation errors

--- sr_dataprep.py
+++ sr_dataprep.py
@@ -1,14 +1,10 @@
 import argparse
 import os
-import sys
-import subprocess
-import tempfile
-import numpy as np
 
 parser = argparse.ArgumentParser(description="Parser for user-specifible dataset paths.")
 
-parser.add_argument("trimmap_dir", type=str, help="The trimmap directory")
-parser.add_argument("dataset_dir", type=str, help="The data directory")
+parser.add_argument("trimmap_dir", type=str, help="The trimmap directory") 
+parser.add_argument("dataset_dir", type=str, help="The data directory") 
 
 args = parser.parse_args()
 
@@ -25,47 +21,47 @@ if(not os.path.exists(write_dir)):
 if(not os.path.exists(flagPath)):
     os.mkdir(flagPath)
 def fil_process_new(filArr,trim,id):
-	idx=0
-	with open(trim,"r") as tfile:
-		for line in tfile:
+    idx=0
+    with open(trim,"r") as tfile:
+        for line in tfile:
 
-			if(line.rstrip()==''):
-				continue
-			elif(line.startswith("m")):
-				continue
-			elif(line.startswith("#")):
-				if line.startswith("#M1"):
-					coords = line.strip().split()[2:5]
-					# print(coords)
+            if(line.rstrip()==''):
+                continue
+            elif(line.startswith("m")):
+                continue
+            elif(line.startswith("#")):
+                if line.startswith("#M1"):
+                    coords = line.strip().split()[2:5]
+                    # print(coords)
 
-				else:
-					continue
-			elif(line.startswith("0.")):
-				sr_orig = open(filArr[idx],"r")
+                else:
+                    continue
+            elif(line.startswith("0.")):
+                sr_orig = open(filArr[idx],"r")
 
 
-				sr = open(os.path.join(write_dir,id+"_"+str(idx)+"_SR.situs"),"w")
-				sr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
-				sr.write(sr_orig.readline())
-				sr.close()
-				sr_orig.close()
+                sr = open(os.path.join(write_dir,id+"_"+str(idx)+"_SR.situs"),"w")
+                sr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
+                sr.write(sr_orig.readline())
+                sr.close()
+                sr_orig.close()
 
-				li = line.split(',')
-				if(len(li)!=2*25*25*25):
-					print("breaking")
-					break
+                li = line.split(',')
+                if(len(li)!=2*25*25*25):
+                    print("breaking")
+                    break
 
-				lr = open(os.path.join(write_dir,id+"_"+str(idx)+"_LR.situs"),"w")
-				lr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
-				lr.write(" ".join(li[0:25*25*25]))
-				lr.close()
+                lr = open(os.path.join(write_dir,id+"_"+str(idx)+"_LR.situs"),"w")
+                lr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
+                lr.write(" ".join(li[0:25*25*25]))
+                lr.close()
 
-				hr = open(os.path.join(write_dir,id+"_"+str(idx)+"_HR.situs"),"w")
-				hr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
-				hr.write(" ".join(li[25*25*25:2*25*25*25]))
-				hr.close()
+                hr = open(os.path.join(write_dir,id+"_"+str(idx)+"_HR.situs"),"w")
+                hr.write("1.000000 "+ coords[0]+" " +coords[1]+" "+coords[2]+" "+"25 25 25\n")
+                hr.write(" ".join(li[25*25*25:2*25*25*25]))
+                hr.close()
 
-				idx+=1
+                idx+=1
 sr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "SR" in x]
 print(dataset_dir)
 
@@ -74,24 +70,24 @@ map_mrc={}
 
 for f in sr_imageFileNames:
 
-	filname=f.split("/")[-1].split(".")[0]
-	key = filname.split("_")[1]
-	idx = int(filname.split("_")[2])
-	# print(key)
-	# print(idx)
-	if key not in map_mrc:
-		ARR = [0]*12100
-		ARR[idx] = f
-		map_mrc[key]=ARR
+    filname=f.split("/")[-1].split(".")[0]
+    key = filname.split("_")[1]
+    idx = int(filname.split("_")[2])
+    # print(key)
+    # print(idx)
+    if key not in map_mrc:
+        ARR = [0]*12100
+        ARR[idx] = f
+        map_mrc[key]=ARR
 
-	else:
-		map_mrc[key][idx] = f
+    else:
+        map_mrc[key][idx] = f
 for i in map_mrc:
-	print(i)
-	newflag=os.path.join(flagPath,i+".flag")
-	print(newflag)
-	if(not os.path.exists(newflag)):
-		os.system("touch "+newflag)
-		trimmap = os.path.join(trimmap_dir,i+"_trimmap")
-		fil_process_new(map_mrc[i],trimmap,i)
-		print(i+" done")
+    print(i)
+    newflag=os.path.join(flagPath,i+".flag")
+    print(newflag)
+    if(not os.path.exists(newflag)):
+        os.system("touch "+newflag)
+        trimmap = os.path.join(trimmap_dir,i+"_trimmap")
+        fil_process_new(map_mrc[i],trimmap,i)
+        print(i+" done")
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import torch
-from torch import nn, optim
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.autograd.variable import Variable
-import os
 import numpy as np
 import argparse
 
@@ -11,78 +9,78 @@ import utils
 from utils import TrainDatasetFromFolder as TDF
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--G_path", help="Generator mdoel path")
-	parser.add_argument("--D_path", help="Discriminator mdoel path")
-	parser.add_argument("--dir_path", help="path to load LR images and store SR images")
-	parser.add_argument("--batch_size", type=int, help="Batch size")
-	parser.add_argument("--res_blocks", type=int, help="No. of resnet blocks")
-	parser.add_argument("--in_channels", type=int, help="No. of input channels")
-	parser.add_argument("--train", type=int, help="Train - 1 or Test - 0")
-	parser.add_argument("--downsample", nargs='?', const=True, default=False, help="Downsampling GAN")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--G_path", help="Generator mdoel path")
+    parser.add_argument("--D_path", help="Discriminator mdoel path")
+    parser.add_argument("--dir_path", help="path to load LR images and store SR images")
+    parser.add_argument("--batch_size", type=int, help="Batch size")
+    parser.add_argument("--res_blocks", type=int, help="No. of resnet blocks")
+    parser.add_argument("--in_channels", type=int, help="No. of input channels")
+    parser.add_argument("--train", type=int, help="Train - 1 or Test - 0")
+    parser.add_argument("--downsample", nargs='?', const=True, default=False, help="Downsampling GAN")
 
-	args = parser.parse_args()
-
-
-	pathG=args.G_path
-	pathD=args.D_path
-	srDir=args.dir_path
-
-	test_batch_size = args.batch_size
-	shuffle_dataset = True
-	random_seed= 42
-
-	root=srDir
-	dataset = TDF(root,25,2,args.downsample,args.train) 
-	dataset_size = len(dataset)
-	print(dataset_size)
-	indices = list(range(dataset_size))
-	if shuffle_dataset :
-	    np.random.seed(random_seed)
-	    np.random.shuffle(indices)
-
-	indices_test=indices
-
-	print(len(indices_test))
-	# Creating PT data samplers and loaders:
-	test_sampler = SubsetRandomSampler(indices_test)
-	test_loader = torch.utils.data.DataLoader(dataset, batch_size=test_batch_size, 
-	                                           sampler=test_sampler)
+    args = parser.parse_args()
 
 
-	# Num batches
-	num_batches=len(test_loader)
-	print(num_batches)
+    pathG=args.G_path
+    pathD=args.D_path
+    srDir=args.dir_path
 
-	G = Generator(args.in_channels,2,args.res_blocks, args.downsample)
-	G.load_state_dict(torch.load(pathG))
-	G.eval()
-	D = Discriminator(args.in_channels)
-	D.load_state_dict(torch.load(pathD))
-	D.eval()
+    test_batch_size = args.batch_size
+    shuffle_dataset = True
+    random_seed= 42
 
-	G.cuda()
-	D.cuda()
+    root=srDir
+    dataset = TDF(root,25,2,args.downsample,args.train) 
+    dataset_size = len(dataset)
+    print(dataset_size)
+    indices = list(range(dataset_size))
+    if shuffle_dataset :
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
 
-	coords=["0","0","0"]
-	with torch.no_grad():
-		for index,(lr, hr, filName) in enumerate(test_loader):
+    indices_test=indices
 
-			lr=lr.float()
-			val_z=Variable(lr)
-			val_z=val_z.cuda()
-			sr_test = G(val_z)
-			val_target=Variable(hr)
-			val_target=val_target.cuda()
-			hr_test=D(val_target).mean()
-			hr_fake=D(sr_test).mean()
+    print(len(indices_test))
+    # Creating PT data samplers and loaders:
+    test_sampler = SubsetRandomSampler(indices_test)
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=test_batch_size, 
+                                               sampler=test_sampler)
 
-			utils.write_voxels(args.batch_size, srDir, sr_test, index, args.downsample, "test",coords,filName)
-			if (index) % 50 == 0: 
-				print(index)
 
-		print(torch.cuda.memory_allocated())
+    # Num batches
+    num_batches=len(test_loader)
+    print(num_batches)
+
+    G = Generator(args.in_channels,2,args.res_blocks, args.downsample)
+    G.load_state_dict(torch.load(pathG))
+    G.eval()
+    D = Discriminator(args.in_channels)
+    D.load_state_dict(torch.load(pathD))
+    D.eval()
+
+    G.cuda()
+    D.cuda()
+
+    coords=["0","0","0"]
+    with torch.no_grad():
+        for index,(lr, hr, filName) in enumerate(test_loader):
+
+            lr=lr.float()
+            val_z=Variable(lr)
+            val_z=val_z.cuda()
+            sr_test = G(val_z)
+            val_target=Variable(hr)
+            val_target=val_target.cuda()
+            hr_test=D(val_target).mean()
+            hr_fake=D(sr_test).mean()
+
+            utils.write_voxels(args.batch_size, srDir, sr_test, index, args.downsample, "test",coords,filName)
+            if (index) % 50 == 0: 
+                print(index)
+
+        print(torch.cuda.memory_allocated())
 
 if __name__=="__main__":
-	main()
+    main()
 
